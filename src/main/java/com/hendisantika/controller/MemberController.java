12038a2f45470d1ee1568dd6c14f0257716c1dd7
hendisantika <hendisantika@yahoo.co.id>
2021-09-03 19:40:20 +0700

    #12 - Add MemberController

---
+++ src/main/java/com/hendisantika/controller/MemberController.java
@@ -0,0 +1,26 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.repository.MemberRepository;
+import com.hendisantika.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-member
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/2021
+ * Time: 7:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class MemberController {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+}
