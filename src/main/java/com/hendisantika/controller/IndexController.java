66fbf72042e23bd9ef672134f4d517754ec773cb
hendisantika <hendisantika@yahoo.co.id>
2021-09-03 19:39:22 +0700

    #11 - Add showIndex in IndexController

--- src/main/java/com/hendisantika/controller/IndexController.java
+++ src/main/java/com/hendisantika/controller/IndexController.java
@@ -1,8 +1,14 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Member;
 import com.hendisantika.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +25,15 @@ public class IndexController {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @GetMapping("/")
+    public String showIndex(Model model, Principal principal, Member member) {
+        if (principal == null) {
+            return "views/loginForm";
+        }
+        model.addAttribute("message", "Hello everyone, we are go to back to Spring with together");
+        model.addAttribute("date", new Date());
+        model.addAttribute("members", memberRepository.getOne(principal.getName()));
+        return "index";
+    }
 }
